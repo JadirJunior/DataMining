3b70f9e67a856e0c2a96c59fbc0a9cc49b4c917e
JadirJunior <jadirjunior8@gmail.com>
2024-08-01 11:17:24 -0300

    Rede Neural

--- Classification/knn_ex.py
+++ Classification/knn_ex.py
@@ -143,6 +143,7 @@ def main():
       'Habitat'
     ]
   
+  
   target = 'Poisonous'
   df = pd.read_csv(input_file,    # Nome do arquivo com dados
                     names = names) # Nome das colunas    
